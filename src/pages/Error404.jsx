69d8acccee1b2497f71ea70dd7eedcc0d2fd8afc
jhonyes04 <jhonyes@hotmail.com>
2026-03-08 19:41:35 +0100

    Modifcada página Error404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,13 +1,20 @@
 import { Link } from 'react-router-dom'; // O 'next/link' si usas Next.js
+import urlImagen404 from '../assets/img/404.png';
 
 export const Error404 = () => {
     return (
-        <div className="container bg-semitransparente rounded-4 text-center centrar-pagina mt-5 p-5">
-            <h1 className="display-1 fw-bold text-warning">404</h1>
-            <h3 className="text-light fw-bold">Página no encontada</h3>
-            <Link to="/" className="link-warning">
-                Volver a la página de inicio
-            </Link>
+        <div className="container bg-semitransparente rounded-4 text-center position-relative mt-5">
+            <img src={urlImagen404} alt="404" className="img-fluid w-50" />
+
+            {/* <h1 className="display-1 fw-bold text-warning">404</h1> */}
+            <div className="position-absolute bottom-0 start-50 translate-middle-x">
+                <h3 className="text-light display-4 fw-bold my-3">
+                    Página no encontada
+                </h3>
+                <Link to="/" className="btn btn-sm btn-warning mb-2">
+                    Volver a home
+                </Link>
+            </div>
         </div>
     );
 };
