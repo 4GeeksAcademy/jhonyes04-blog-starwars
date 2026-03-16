dc32370b11e2f0808b66a6ac74cda2d34bbaa145
jhonyes04 <jhonyes@hotmail.com>
2026-03-16 10:50:30 +0100

    algunos cambios

--- src/pages/Busquedas.jsx
+++ src/pages/Busquedas.jsx
@@ -5,12 +5,15 @@ export const Busquedas = () => {
     const { store } = useGlobalReducer();
     const { busquedas } = store;
 
+    const total = busquedas.length;
+
     return (
         <>
             <CardList
-                titulo={`Resultados de búsqueda ${busquedas.length}`}
+                titulo="Resultados de búsqueda"
                 elementos={busquedas}
                 tipo="characters"
+                total={total}
             />
         </>
     );
